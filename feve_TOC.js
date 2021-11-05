b67078cae90a7b81a40af82a631d8e83a2ecefe3
Andrea-FEVE <85485500+Andrea-FEVE@users.noreply.github.com>
2021-11-05 06:17:30 +0100

    Create feve_TOC.js

---
+++ feve_TOC.js
@@ -0,0 +1,20 @@
+document.getElementById("content").querySelectorAll(TocField).forEach(function(heading, i) { // runs a function for all headings inside your rich text element
+  heading.setAttribute("id", "toc-" + i); // gives each heading a unique id
+  heading.classList.add("content-h");
+  const item = document.createElement("a"); // creates an anchor element called "item" for each heading
+  item.innerHTML = heading.innerHTML; // gives each item the text of the corresponding heading
+  (TocField).split(",").forEach(function(x) { // runs a function for each item in your headings list
+    if (heading.tagName.toLowerCase()==x) {
+      item.classList.add("tocitem", "toc-" + x); // gives each item the correct class
+    }
+  });
+  
+  item.setAttribute("onclick", "scrollTarget(\'" + heading.id + "\')");
+  document.querySelector("#toc").appendChild(item); // places each item inside the Table of Contents div
+});
+
+function scrollTarget(tgt) { 
+var target = document.getElementById(tgt);
+var negoffset = window.innerHeight/2;
+window.scrollTo({top: (target.offsetTop + negoffset), behavior: "smooth"})
+};
