1cc94d76a4a4f727b25b68a9f2d9f78e94d0181e
Alexandre Macé <34915784+alexandre-mace@users.noreply.github.com>
2023-02-02 15:33:36 +0100

    Update feve_TOC.js

--- feve_TOC.js
+++ feve_TOC.js
@@ -20,6 +20,6 @@ document.getElementById("content").querySelectorAll(TocField).forEach(function(h
 
 function scrollTarget(tgt) { 
 var target = document.getElementById(tgt);
-var negoffset = window.innerHeight/6;
+var negoffset = window.innerHeight/0.5;
 window.scrollTo({top: (target.offsetTop + negoffset), behavior: "smooth"})
 };
