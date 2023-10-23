7835283712fed90067584a1c2304cc3e02ad9be0
alexandre-mace <helloalexandremace@protonmail.com>
2023-10-23 17:40:05 +0200

    Add elevage modal for website

---
+++ website/global/elevageModal.js
@@ -0,0 +1,10 @@
+const elevageModal = document.querySelector('#elevage-2023')
+if (elevageModal && !localStorage.getItem('elevage-2023') && !document.location.pathname.includes('photos-fermes')) {
+    MicroModal.show('elevage-2023', {
+        onClose: (modal) => {
+            localStorage.setItem('elevage-2023', 'true')
+        }
+    })
+    document.querySelector('#elevage-cta').addEventListener('click', (e) => MicroModal.close('elevage-2023'))
+
+}
