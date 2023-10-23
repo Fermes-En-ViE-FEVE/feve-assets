356a59f89faf526aecc5f2dd9052b326ac3c5a52
alexandre-mace <helloalexandremace@protonmail.com>
2023-10-23 13:22:37 +0200

    Update display nps script

--- lagrange/nps/displayNps.js
+++ lagrange/nps/displayNps.js
@@ -1,5 +1,9 @@
 MemberStack.onReady.then(member => {
     if (member.loggedIn) {
+        localStorage.setItem('visitedPages', (localStorage.getItem('visitedPages') ? parseInt(localStorage.getItem('visitedPages')) : 0) + 1)
+        // if (document.querySelector('#banner')) {
+        //     document.querySelector('.banner-button').setAttribute("href", (document.querySelector('.banner-button').getAttribute("href") + '?email=' + member["email"]))
+        // }
         setTimeout(() => {
             if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
                 document.querySelector('#nps-wrapper').classList.add('show-nps')
