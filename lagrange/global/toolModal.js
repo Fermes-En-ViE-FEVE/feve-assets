6b53241e111ddd67f979e9abe8a3df9799c0f6fb
alexandre-mace <helloalexandremace@protonmail.com>
2023-10-23 13:19:43 +0200

    Remove console logs

--- lagrange/global/toolModal.js
+++ lagrange/global/toolModal.js
@@ -1,10 +1,8 @@
 MemberStack.onReady.then(member => {
     const toolModal = document.querySelector('#tool-presentation-modal')
     if (toolModal && !localStorage.getItem(toolModal.getAttribute('tool-name'))) {
-        console.log(localStorage.getItem(toolModal.getAttribute('tool-name')))
         MicroModal.show('tool-presentation-modal', {
             onClose: (modal) => {
-                console.log('closing modal')
                 localStorage.setItem(toolModal.getAttribute('tool-name'), 'true')
             }
         })
