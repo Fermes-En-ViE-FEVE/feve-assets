ad84086ef69d77f38703115f2dfe46b2c5c70db4
alexandre-mace <helloalexandremace@protonmail.com>
2023-10-23 13:37:40 +0200

    Update reset password script

--- lagrange/memberstack/resetPassword.js
+++ lagrange/memberstack/resetPassword.js
@@ -1,7 +1,6 @@
 MemberStack.onReady.then(member => {
     setTimeout(() => {
         const resetPasswordLabel = document.querySelector("label[ms-lang='reset_reset_code']");
-        console.log(resetPasswordLabel)
         if (resetPasswordLabel) {
             resetPasswordLabel.innerHTML = 'Entrez le code de réinitialisation'
         }
