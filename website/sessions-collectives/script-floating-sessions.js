5e423d28c51b410d945f69b3c723fe83a9a0ad8c
alexandre-mace <helloalexandremace@protonmail.com>
2023-10-27 16:12:21 +0200

    Update floating session script

--- website/sessions-collectives/script-floating-sessions.js
+++ website/sessions-collectives/script-floating-sessions.js
@@ -20,3 +20,7 @@ const sessionsCollectivesButtons = document.querySelectorAll('.sessions-collecti
     MicroModal.show('sessions-collectives-modal');
   })
 })
+function show_Form() {document.getElementById('form').style.display='block';}
+if (document.querySelector('#sessions-collectives-modal')) {
+  getEventsInTable('Investisseur');document.getElementById('date_sessions').onclick=show_Form;
+}
