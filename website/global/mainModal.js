e424ddbb57224c07ccc4a80b819fd55b8b999d01
alexandre-mace <helloalexandremace@protonmail.com>
2023-11-13 10:58:04 +0100

    Update main modal for 10 millions modal

---
+++ website/global/mainModal.js
@@ -0,0 +1,9 @@
+const mainModal = document.querySelector('#main-modal')
+if (mainModal && !localStorage.getItem('millions') && !document.location.pathname.includes('photos-fermes')) {
+    MicroModal.show('main-modal', {
+        onClose: (modal) => {
+            localStorage.setItem('millions', 'true')
+        }
+    })
+    document.querySelector('#main-modal-cta').addEventListener('click', (e) => MicroModal.close('main-modal'))
+}
