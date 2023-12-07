917813c75cb2eafa3f3434417ea6dc0c73072fbe
Marion Brochet <brochetmarion@gmail.com>
2023-12-07 14:30:09 +0100

    Update elevageModal.js

--- lagrange/global/elevageModal.js
+++ lagrange/global/elevageModal.js
@@ -1,11 +1,11 @@
 MemberStack.onReady.then(member => {
     const elevageModal = document.querySelector('#elevage-2023')
-    if (elevageModal && !localStorage.getItem('enquete-femme-agriculture') && member.loggedIn && member.email !== 'alexandre.mace+2@feve.co') {
-        MicroModal.show('enquete-femme-agriculture', {
+    if (elevageModal && !localStorage.getItem('elevage-2023') && member.loggedIn && member.email !== 'alexandre.mace+2@feve.co') {
+        MicroModal.show('elevage-2023', {
             onClose: (modal) => {
-                localStorage.setItem('enquete-femme-agriculture', 'true')
+                localStorage.setItem('elevage-2023', 'true')
             }
         })
-        document.querySelector('#elevage-cta').addEventListener('click', (e) => MicroModal.close('enquete-femme-agriculture'))
+        document.querySelector('#elevage-cta').addEventListener('click', (e) => MicroModal.close('elevage-2023'))
     }
 })
