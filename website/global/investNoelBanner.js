cdd1ef81fd00282b2fe31400a41057e13086e002
alexandre-mace <helloalexandremace@protonmail.com>
2023-12-08 10:52:58 +0100

    Update invest noel banner

--- website/global/investNoelBanner.js
+++ website/global/investNoelBanner.js
@@ -3,3 +3,16 @@ addEventListener("DOMContentLoaded", (event) => {
         headings: ["Jours", "Heures", "Minutes", "Secondes"],
     }).start();
 });
+if (!localStorage.getItem('noel-banner-2023')) {
+    document.querySelector('.bandeau-invest-noel').style.display = 'flex'
+    document.querySelector('.bandeau-invest-noel-close').addEventListener('click', () => {
+       closeBanner()
+    })
+    document.querySelector('.bandeau-invest-noel a').addEventListener('click', () => {
+        closeBanner()
+    })
+}
+const closeBanner = () => {
+    document.querySelector('.bandeau-invest-noel').style.display = 'flex'
+    localStorage.setItem('noel-banner-2023', 'true')
+}
