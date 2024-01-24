f9f338c0aef24c1e7a7146d6e5c199ea393fc448
alexandre-mace <helloalexandremace@protonmail.com>
2024-01-24 12:03:18 +0100

    Update nps

--- lagrange/nps/displayNps.js
+++ lagrange/nps/displayNps.js
@@ -8,6 +8,7 @@ MemberStack.onReady.then(member => {
             if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
                 document.querySelector('#nps-wrapper').classList.add('show-nps')
                 document.querySelector('#nps-wrapper button').setAttribute("data-tf-hidden", ("email=" + member["email"]))
+                window.tf.createWidget('01HMXGFAZRKPJPP124X1HMC3ZQ')
             }
         }, 10000)
         document.querySelector('#nps-wrapper button').addEventListener('click', () => {
