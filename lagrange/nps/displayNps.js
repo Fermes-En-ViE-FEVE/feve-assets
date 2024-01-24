bcf3f49498ef2c5457212750363dbac5477b5a4b
alexandre-mace <helloalexandremace@protonmail.com>
2024-01-24 14:11:44 +0100

    Update nps

--- lagrange/nps/displayNps.js
+++ lagrange/nps/displayNps.js
@@ -4,12 +4,13 @@ MemberStack.onReady.then(member => {
         // if (document.querySelector('#banner')) {
         //     document.querySelector('.banner-button').setAttribute("href", (document.querySelector('.banner-button').getAttribute("href") + '?email=' + member["email"]))
         // }
-        setTimeout(() => {
-            if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
-                document.querySelector('#nps-wrapper').classList.add('show-nps')
-                document.querySelector('#nps-wrapper button').setAttribute("data-tf-hidden", ("email=" + member["email"]))
-            }
-        }, 10000)
+        if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
+            document.querySelector('#nps-wrapper button').setAttribute("data-tf-hidden", ("email=" + member["email"]))
+            setTimeout(() => {
+                    document.querySelector('#nps-wrapper').classList.add('show-nps')
+            }, 10000)
+        }
+
         document.querySelector('#nps-wrapper button').addEventListener('click', () => {
             setTimeout(() => {
                 localStorage.setItem('nps', 'true')
