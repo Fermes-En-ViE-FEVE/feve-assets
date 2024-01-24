d591cfaaa3f6f38f6d67268e14404ecf162f48aa
alexandre-mace <helloalexandremace@protonmail.com>
2024-01-24 14:07:41 +0100

    Update nps

--- lagrange/nps/displayNps.js
+++ lagrange/nps/displayNps.js
@@ -4,21 +4,16 @@ MemberStack.onReady.then(member => {
         // if (document.querySelector('#banner')) {
         //     document.querySelector('.banner-button').setAttribute("href", (document.querySelector('.banner-button').getAttribute("href") + '?email=' + member["email"]))
         // }
-        const { refresh } = window.tf.createSlider('tE1MgIrN', {
-            hidden: {
-                email:  member["email"],
-            },
-        })
         setTimeout(() => {
             if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
                 document.querySelector('#nps-wrapper').classList.add('show-nps')
+                document.querySelector('#nps-wrapper button').setAttribute("data-tf-hidden", ("email=" + member["email"]))
             }
         }, 10000)
-        document.querySelector('#nps-wrapper #nps').addEventListener('click', () => {
-            refresh()
+        document.querySelector('#nps-wrapper button').addEventListener('click', () => {
             setTimeout(() => {
                 localStorage.setItem('nps', 'true')
-                document.querySelector('#nps-wrapper #nps').style.display = "none";
+                document.querySelector('#nps-wrapper button').style.display = "none";
             }, 2000)
         })
     }
