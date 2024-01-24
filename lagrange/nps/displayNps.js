cd0bc6aee9f8017620fffd111c9f17c83838bac8
alexandre-mace <helloalexandremace@protonmail.com>
2024-01-24 13:08:17 +0100

    Update nps

--- lagrange/nps/displayNps.js
+++ lagrange/nps/displayNps.js
@@ -4,14 +4,14 @@ MemberStack.onReady.then(member => {
         // if (document.querySelector('#banner')) {
         //     document.querySelector('.banner-button').setAttribute("href", (document.querySelector('.banner-button').getAttribute("href") + '?email=' + member["email"]))
         // }
+        window.tf.createSlider('tE1MgIrN', {
+            hidden: {
+                email:  member["email"],
+            },
+        })
         setTimeout(() => {
             if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
                 document.querySelector('#nps-wrapper').classList.add('show-nps')
-                window.tf.createSlider('tE1MgIrN', {
-                    hidden: {
-                        email:  member["email"],
-                    },
-                })
             }
         }, 10000)
         document.querySelector('#nps-wrapper #nps').addEventListener('click', () => {
