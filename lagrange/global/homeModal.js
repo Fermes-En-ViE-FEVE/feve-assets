62a03f7e9322587654b67a3ba8e6fb566dbe60a9
Marion Brochet <brochetmarion@gmail.com>
2024-02-05 11:57:05 +0100

    Update homeModal.js

--- lagrange/global/homeModal.js
+++ lagrange/global/homeModal.js
@@ -1,6 +1,6 @@
 MemberStack.onReady.then(member => {
-    const elevageModal = document.querySelector('#modal-home-lg')
-    if (elevageModal && !localStorage.getItem('appel-candidature-myrtille') && member.loggedIn && member.email !== 'alexandre.mace+2@feve.co') {
+    const homeModal = document.querySelector('#modal-home-lg')
+    if (homeModal && !localStorage.getItem('appel-candidature-myrtille') && member.loggedIn && member.email !== 'alexandre.mace+2@feve.co') {
         MicroModal.show('modal-home-lg', {
             onClose: (modal) => {
                 localStorage.setItem('appel-candidature-myrtille', 'true')
