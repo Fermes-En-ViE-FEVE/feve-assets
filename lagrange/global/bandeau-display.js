999519722da3de1137bb89520d02252748b590a6
Marion Brochet <brochetmarion@gmail.com>
2024-02-15 10:20:19 +0100

    Update bandeau-display.js

--- lagrange/global/bandeau-display.js
+++ lagrange/global/bandeau-display.js
@@ -1,4 +1,4 @@
-if (window.screen.width > 768 && !localStorage.getItem('bandeau-appel-offre-02-24') {
+if (window.screen.width > 768 && !localStorage.getItem('bandeau-appel-offre-02-24')) {
   document.querySelector('#bandeau-appel-offre').style.display = 'block'
   document.querySelector('appel-offre-close').addEventListener('click', () => {
     localStorage.setItem('bandeau-appel-offre-02-24', 'true')
