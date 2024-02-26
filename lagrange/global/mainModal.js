754ae41727e42609ff546dccbc171ea4d952f43e
alexandre-mace <helloalexandremace@protonmail.com>
2024-02-26 17:14:56 +0100

    Update main modal

--- lagrange/global/mainModal.js
+++ lagrange/global/mainModal.js
@@ -1,18 +1,15 @@
 const isMainModalActive = true
 let mainModalName = 'main-modal-SIA-2024'
 
-console.log('hi')
+MemberStack.onReady.then(member => {
     if (isMainModalActive && document.querySelector('#main-modal')  && (window.location.pathname === '/' ||
-        window.location.pathname === '') && !localStorage.getItem(mainModalName)) {
+        window.location.pathname === '') && !localStorage.getItem(mainModalName) && member.email !== 'alexandre.mace+2@feve.co') {
         MicroModal.show('main-modal', {
             onClose: (modal) => {
                 localStorage.setItem(mainModalName, 'true')
             }
         })
-        console.log("modal")
         document.querySelector('#main-modal-cta').addEventListener('click', (e) => MicroModal.close('main-modal'))
-        document.querySelector('#main-modal-close').addEventListener('click', (e) => {
-            console.log('hiding modal')
-            MicroModal.close('main-modal')
-        })
+        document.querySelector('#main-modal-close').addEventListener('click', (e) => MicroModal.close('main-modal'))
     }
+})
