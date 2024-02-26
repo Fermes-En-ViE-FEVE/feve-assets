9361e9ec30437e03ad12c7ca53c545191bce6d9b
alexandre-mace <helloalexandremace@protonmail.com>
2024-02-26 15:20:16 +0100

    Update bandeau display

--- website/global/bandeauDisplay.js
+++ website/global/bandeauDisplay.js
@@ -8,12 +8,14 @@ if (isBandeauActive && document.querySelector('#bandeau') && window.screen.width
   if (document.querySelector('#bandeau-close')){
     document.querySelector('#bandeau-close').addEventListener('click', () => {
       localStorage.setItem(currentBandeauName, 'true')
+      document.querySelector('#bandeau').style.display = 'none'
     })
   }
 
   if (document.querySelector('#bandeau-cta')){
     document.querySelector('#bandeau-cta').addEventListener('click', () => {
       localStorage.setItem(currentBandeauName, 'true')
+      document.querySelector('#bandeau').style.display = 'none'
     })
   }
 }
