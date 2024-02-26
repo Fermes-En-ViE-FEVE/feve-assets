aaa04866fe61afe6ffa28b57370ce7f350cad206
alexandre-mace <helloalexandremace@protonmail.com>
2024-02-26 15:19:59 +0100

    Update bandeau display

--- lagrange/global/bandeauDisplay.js
+++ lagrange/global/bandeauDisplay.js
@@ -1,18 +1,20 @@
 let isBandeauActive = true
 let currentBandeauName = 'bandeau-SIA-2024'
 
-if (isBandeauActive && document.querySelector('#bandeau') &&  window.screen.width > 768 && !localStorage.getItem(currentBandeauName) ) {
+if (isBandeauActive && document.querySelector('#bandeau') &&  window.screen.width > 768 && !localStorage.getItem(currentBandeauName)) {
   document.querySelector('#bandeau').style.display = 'block'
 
   if (document.querySelector('#bandeau-close')){
     document.querySelector('#bandeau-close').addEventListener('click', () => {
       localStorage.setItem(currentBandeauName, 'true')
+      document.querySelector('#bandeau').style.display = 'none'
     })
   }
 
   if (document.querySelector('#bandeau-cta')){
     document.querySelector('#bandeau-cta').addEventListener('click', () => {
       localStorage.setItem(currentBandeauName, 'true')
+      document.querySelector('#bandeau').style.display = 'none'
     })
   }
 }
