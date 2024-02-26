dab36d4fa3ae8da7518219ee86e0041377caec9f
alexandre-mace <helloalexandremace@protonmail.com>
2024-02-26 15:43:02 +0100

    Update main modal feve.co

--- website/global/mainModal.js
+++ website/global/mainModal.js
@@ -1,22 +1,22 @@
-const isMainModalActive = false
-const mainModal = document.querySelector('#main-modal')
+const isMainModalActive = true
+let mainModalName = 'main-modal-SIA-2024'
+
 if (
     isMainModalActive &&
-    mainModal &&
-    !localStorage.getItem('millions') &&
+    document.querySelector('#main-modal') &&
+    !localStorage.getItem(mainModalName) &&
     !document.location.pathname.includes('photos-fermes') &&
     !document.location.pathname.includes('derniere-ferme')
 ) {
     if (
-        window.location.href.includes('investir') ||
-        window.location.href.includes('offrir-des-actions') ||
-        window.location.href.includes('cadeau') ||
-        window.location.href.includes('ressources-invest')
-
+        !window.location.href.includes('investir') &&
+        !window.location.href.includes('offrir-des-actions') &&
+        !window.location.href.includes('cadeau') &&
+        !window.location.href.includes('ressources-invest')
     ) {
         MicroModal.show('main-modal', {
             onClose: (modal) => {
-                localStorage.setItem('millions', 'true')
+                localStorage.setItem(mainModalName, 'true')
             }
         })
         document.querySelector('#main-modal-cta').addEventListener('click', (e) => MicroModal.close('main-modal'))
