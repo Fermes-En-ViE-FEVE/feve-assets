320cb7bfec4d83ae2e3ac958d1024b357dec3b14
alexandre-mace <helloalexandremace@protonmail.com>
2024-04-11 17:55:27 +0200

    Update scroll tarfet

--- website/global/tableOfContent.js
+++ website/global/tableOfContent.js
@@ -22,6 +22,6 @@ document.getElementById("content").querySelectorAll(TocField).forEach(function(h
 
 function scrollTarget(tgt) { 
 var target = document.getElementById(tgt);
-var negoffset = 650;
+var negoffset = 550;
 window.scrollTo({top: (target.offsetTop + negoffset), behavior: "smooth"})
 };
