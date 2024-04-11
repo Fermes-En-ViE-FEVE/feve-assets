f3ebf3e35aa16ca233b6e7183be12e0fe10549a8
alexandre-mace <helloalexandremace@protonmail.com>
2024-04-11 10:45:14 +0200

    Comment nps script

--- lagrange/nps/displayNps.js
+++ lagrange/nps/displayNps.js
@@ -1,21 +1,18 @@
-MemberStack.onReady.then(member => {
-    if (member.loggedIn) {
-        localStorage.setItem('visitedPages', (localStorage.getItem('visitedPages') ? parseInt(localStorage.getItem('visitedPages')) : 0) + 1)
-        // if (document.querySelector('#banner')) {
-        //     document.querySelector('.banner-button').setAttribute("href", (document.querySelector('.banner-button').getAttribute("href") + '?email=' + member["email"]))
-        // }
-        if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
-            document.querySelector('#nps-wrapper button').setAttribute("data-tf-hidden", ("email=" + member["email"]))
-            setTimeout(() => {
-                    document.querySelector('#nps-wrapper').classList.add('show-nps')
-            }, 10000)
-        }
-
-        document.querySelector('#nps-wrapper button').addEventListener('click', () => {
-            setTimeout(() => {
-                localStorage.setItem('nps', 'true')
-                document.querySelector('#nps-wrapper button').style.display = "none";
-            }, 2000)
-        })
-    }
-})
+// MemberStack.onReady.then(member => {
+//     if (member.loggedIn) {
+//         localStorage.setItem('visitedPages', (localStorage.getItem('visitedPages') ? parseInt(localStorage.getItem('visitedPages')) : 0) + 1)
+//         if (document.querySelector('#nps-wrapper') && !localStorage.getItem('nps') && (localStorage.getItem('visitedPages') && parseInt(localStorage.getItem('visitedPages')) > 10)) {
+//             document.querySelector('#nps-wrapper button').setAttribute("data-tf-hidden", ("email=" + member["email"]))
+//             setTimeout(() => {
+//                     document.querySelector('#nps-wrapper').classList.add('show-nps')
+//             }, 10000)
+//         }
+//
+//         document.querySelector('#nps-wrapper button').addEventListener('click', () => {
+//             setTimeout(() => {
+//                 localStorage.setItem('nps', 'true')
+//                 document.querySelector('#nps-wrapper button').style.display = "none";
+//             }, 2000)
+//         })
+//     }
+// })
