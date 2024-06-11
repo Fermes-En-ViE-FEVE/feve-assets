483e674d8754b0a7162c21a3bcf79aa5d41aa265
Andrea-FEVE <85485500+Andrea-FEVE@users.noreply.github.com>
2024-06-11 12:02:18 +0200

    commented lines that ignore home

--- lagrange/tracking/entryPage.js
+++ lagrange/tracking/entryPage.js
@@ -5,8 +5,8 @@ if (
     !window.location.pathname.includes('onboarding') &&
     !window.location.pathname.includes('redirect') &&
     !window.location.pathname.includes('oups') &&
-    window.location.pathname !== '/' &&
-    window.location.pathname !== ''
+    //window.location.pathname !== '/' &&
+    //window.location.pathname !== ''
 ) {
     localStorage.setItem('grange_entry_page', window.location.href)
     console.log('grange_entry_page', window.location.href)
