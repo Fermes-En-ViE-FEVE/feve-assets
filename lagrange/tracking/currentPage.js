494bccb77be6e7f82fb5a6030a7d38f19606cb93
Andrea-FEVE <85485500+Andrea-FEVE@users.noreply.github.com>
2024-07-02 12:29:33 +0200

    remove exclusion for home and "trouver-ferme-inscription"

--- lagrange/tracking/currentPage.js
+++ lagrange/tracking/currentPage.js
@@ -3,10 +3,7 @@ if (
     !window.location.pathname.includes('/inscription') &&
     !window.location.pathname.includes('onboarding') &&
     !window.location.pathname.includes('redirect') &&
-    !window.location.pathname.includes('oups') &&
-    !window.location.pathname.includes('trouver-ferme-inscription') &&
-    window.location.pathname !== '/' &&
-    window.location.pathname !== ''
+    !window.location.pathname.includes('oups')
 ) {
     sessionStorage.setItem("grange_current_url", window.location.href)
     console.log('grange_current_url', window.location.href)
