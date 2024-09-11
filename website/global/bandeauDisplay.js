f46e0b0cbb5d0b8cd2d406c201c286d24ed6d786
alexandre-mace <helloalexandremace@protonmail.com>
2024-09-11 15:16:05 +0200

    feat(bandeau website): handle pp only option

--- website/global/bandeauDisplay.js
+++ website/global/bandeauDisplay.js
@@ -1,7 +1,22 @@
 let isBandeauActive = false
 let currentBandeauName = 'bandeau-offre-ver-de-terre-2024'
 let preprodOnly = false
+let ppOnly = true
 const isIframe = window.self !== window.top;
+
+const ppUrls = [
+  'https://www.feve.co/',
+  'https://www.feve.co',
+  'https://www.feve.co/installation-agricole',
+  'https://www.feve.co/je-cherche-une-ferme-a-reprendre',
+  'https://www.feve.co/je-finance-ma-ferme',
+  'https://www.feve.co/installation-agricole/sessions-d-information',
+  'https://www.feve.co/les-feves-financees',
+  'https://www.feve.co/ressources-installation',
+  'https://www.feve.co/guides-pratiques/guide-de-linstallation-agricole',
+  'https://www.feve.co/guides-pratiques/guide-des-aides-installation',
+  'https://www.feve.co/guides-pratiques/guide-de-la-formation-agricole'
+];
 
 if (
     (preprodOnly && window.location.href.includes("new-feve") || !preprodOnly) &&
@@ -10,6 +25,7 @@ if (
     !localStorage.getItem(currentBandeauName) &&
     !window.location.href.includes('feve.co/evenements/') &&
     !window.location.href.includes('embed') &&
+    (ppUrls.includes(window.location.href) || !ppOnly) &&
     !isIframe
 ) {
   document.querySelector('#bandeau').style.display = 'block'
