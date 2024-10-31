eeeba98f1b277dddb8cee82f7ef023f9490f08a7
Alexandre Macé <helloalexandremace@protonmail.com>
2024-10-31 18:42:32 +0100

    feat(website toc): update UI

--- website/global/tableOfContent.js
+++ website/global/tableOfContent.js
@@ -3,7 +3,7 @@ document.getElementById("content").querySelectorAll(TocField).forEach(function(h
   heading.setAttribute("id", "toc-" + i); // gives each heading a unique id
   heading.classList.add("content-h");
   const item = document.createElement("a"); // creates an anchor element called "item" for each heading
-  item.innerHTML = heading.innerHTML; // gives each item the text of the corresponding heading
+  item.innerText = heading.innerText; // gives each item the text of the corresponding heading
   (TocField).replace(/\s/g, '').split(",").forEach(function(x) { // runs a function for each item in your headings list
     if (heading.tagName.toLowerCase()==x) {
       item.classList.add("tocitem", "toc-" + x); // gives each item the correct class
