c85d5394ec4c6f53d4ef5952b0670cf52f0c1419
Alexandre Macé <helloalexandremace@protonmail.com>
2024-11-27 15:03:00 +0100

    feat(farm modal): add logs

--- lagrange/global/farmModalTriggers.js
+++ lagrange/global/farmModalTriggers.js
@@ -1,5 +1,7 @@
 const farmModalModalButtons = document.querySelectorAll('.farm-form-modal-trigger').forEach((button) => {
     button.addEventListener('click', (e) => {
+        console.log('opening modal')
+        console.log(document.getElementById("farm-form-modal"))
         MicroModal.show('farm-form-modal');
     })
 })
