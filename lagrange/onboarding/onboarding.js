9bd8ae4c745e9c5e135ef0ee183aee7c8f12ff1f
Alexandre Macé <helloalexandremace@protonmail.com>
2024-11-27 18:19:13 +0100

    feat(onboarding): only trigger onboarding modal if it exist

--- lagrange/onboarding/onboarding.js
+++ lagrange/onboarding/onboarding.js
@@ -1,15 +1,15 @@
 MemberStack.onReady.then(member => {
-    if (member.loggedIn && !['', '/', '/redirect'].includes(window.location.pathname) && (!member.onboardingModal || window.location.href.includes('presentation=true'))) {
+    if (
+        document.querySelector('#modal-onboarding') && member.loggedIn && !['', '/', '/redirect'].includes(window.location.pathname) && (!member.onboardingModal || window.location.href.includes('presentation=true'))
+    ) {
         if (window.location.href.includes('presentation=true')) {
             window.history.replaceState({}, document.title, "/" + "accueil");
         }
-        if (document.querySelector('#modal-onboarding')) {
             MicroModal.show('modal-onboarding', {
                 onClose: (modal) => {
                     member.updateProfile({onboardingModal: "true"}, false)
                 }
             });
-        }
         document.querySelector('#slider-last-next').addEventListener('click', (e) => {
             member.updateProfile({onboardingModal: "true"}, false)
         })
